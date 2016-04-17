96bd104d6bacf8dc2e2f2588cecc27e3735e8b9b
Andrey <frost86@inbox.ru>
2016-04-17 11:50:18 +0300

    оценки

--- speech_accent/app/src/main/java/andyanika/speechaccent/fragments/RatingFragment.java
+++ speech_accent/app/src/main/java/andyanika/speechaccent/fragments/RatingFragment.java
@@ -12,11 +12,15 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import andyanika.speechaccent.MainActivity;
 import andyanika.speechaccent.OnChangeFragmentListener;
 import andyanika.speechaccent.R;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
 
 /**
@@ -46,6 +50,66 @@ public class RatingFragment extends DialogFragment {
     void onOkClicked() {
         getDialog().dismiss();
         changeFragmentListener.onChange(MainActivity.FRAGMENT_MAIN);
+    }
+
+    @InjectView(R.id.star_1)
+    ImageView star1;
+
+    @InjectView(R.id.star_2)
+    ImageView star2;
+
+    @InjectView(R.id.star_3)
+    ImageView star3;
+
+    @InjectView(R.id.star_4)
+    ImageView star4;
+
+    @InjectView(R.id.star_5)
+    ImageView star5;
+
+    @OnClick(R.id.star_1)
+    void onStar1Clicked() {
+        star1.setBackgroundResource(R.drawable.star);
+        star2.setBackgroundResource(R.drawable.star_gr);
+        star3.setBackgroundResource(R.drawable.star_gr);
+        star4.setBackgroundResource(R.drawable.star_gr);
+        star5.setBackgroundResource(R.drawable.star_gr);
+    }
+
+    @OnClick(R.id.star_2)
+    void onStar2Clicked() {
+        star1.setBackgroundResource(R.drawable.star);
+        star2.setBackgroundResource(R.drawable.star);
+        star3.setBackgroundResource(R.drawable.star_gr);
+        star4.setBackgroundResource(R.drawable.star_gr);
+        star5.setBackgroundResource(R.drawable.star_gr);
+    }
+
+    @OnClick(R.id.star_3)
+    void onStar3Clicked() {
+        star1.setBackgroundResource(R.drawable.star);
+        star2.setBackgroundResource(R.drawable.star);
+        star3.setBackgroundResource(R.drawable.star);
+        star4.setBackgroundResource(R.drawable.star_gr);
+        star5.setBackgroundResource(R.drawable.star_gr);
+    }
+
+    @OnClick(R.id.star_4)
+    void onStar4Clicked() {
+        star1.setBackgroundResource(R.drawable.star);
+        star2.setBackgroundResource(R.drawable.star);
+        star3.setBackgroundResource(R.drawable.star);
+        star4.setBackgroundResource(R.drawable.star);
+        star5.setBackgroundResource(R.drawable.star_gr);
+    }
+
+    @OnClick(R.id.star_5)
+    void onStar5Clicked() {
+        star1.setBackgroundResource(R.drawable.star);
+        star2.setBackgroundResource(R.drawable.star);
+        star3.setBackgroundResource(R.drawable.star);
+        star4.setBackgroundResource(R.drawable.star);
+        star5.setBackgroundResource(R.drawable.star);
     }
 
     @OnClick(R.id.btn_cancel)
